41a60761f254cf94436a6fed23f785d3c6a37818
Dhaha Firas <firas.dhaha@student.ecp.fr>
2021-02-06 15:26:23 +0100

    Change error message

--- backend/server.py
+++ backend/server.py
@@ -13,13 +13,19 @@ def get_response():
         text = "hello world"
         return jsonify(text)
     else :
-        error_message = "You have reloaded the page, hence forcing the backend server to send this message. \n"
-        error_message+= "Please close the app with ctrl-c (or command-c) in the terminal on both the server and the app \n"
-        error_message+= "And restart it in you terminal using :\n"
-        error_message+= "** in movie-app : yarn start \n"
-        error_message+= "** in back end: flask run -h localhost -p 3000"
-        error_message+= "\n \n \n Our apologies for the inconvenience"
-        return jsonify(error_message)
+        error_message = """<html><body><center>"""
+        error_message = """<center><h2>OUPS YOU HAVE RELOADED THE PAGE</h2></center>"""
+        error_message+= """<p>Please close the app with ctrl-c (or command-c) in the terminal on both the server and the app </p>"""
+        error_message+= """<p>And restart it in you terminal using :</p>"""
+        error_message+= """<h3>in movie-app : </h3>"""
+        error_message+= """<p><code>yarn start </code></p>"""
+        error_message+= """<h3>in backend: </h3>"""
+        error_message+= """<p><code>flask run -h localhost -p 3000 </code></p>"""
+        error_message+= """<p></p>"""
+        error_message+= """<p></p>"""
+        error_message+= """<center><h3>Our apologies for the inconvenience</h3></center>"""
+        error_message += """</center></body></html>"""
+        return error_message
 
 if __name__ == '__main__':
     app.run(host="localhost", port=3000, debug=True)
